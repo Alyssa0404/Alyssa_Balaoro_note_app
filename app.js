f9c4e10155bb9d9341729f85d6b519337fd59aef
Alyssa_Balaoro <balaoroalyssa@gmail.com>
2022-10-17 19:42:25 +0800

    Update update.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-//const write = require ('./write');
-//const cmd = process.argv[2]
-//write(cmd);
-
-//const read = require('./read');
-//console.log(read());
 const read = require('./read')
 const add = require('./add')
 const write = require('./write')
@@ -12,19 +6,16 @@ const data = process.argv
 var note = {}
 
 if(data[2] == 'add') {
-    // Build Object
     note = { 
         id: data[3],
         title: data[4],
         body: data[5]
     }
- // Get old note value
  var oldNote = read()
- // Add note to note.txt
  add(note, oldNote)
 }
 if(data[2] == 'read') {
-    // Import present
+
     const present = require('./present')
     present(read()) }
 if (data[2] == 'delete') {
@@ -35,18 +26,18 @@ if (data[2] == 'delete') {
     remove (id,oldNote)
     console.log(read())
 }
-// var note = {}
-// let update = require('./update') 
-// if (data[2] == 'update') { 
-//    let note = { 
-//         id : data [3],
-//         title: data[4],
-//         body: data[5]
-//     }
+var note = {}
+let update = require('./update') 
+if (data[2] == 'update') { 
+   let note = { 
+        id : data [3],
+        title: data[4],
+        body: data[5]
+    }
     
  
-//  var oldNote = read()
+ var oldNote = read()
 
-//  update(note,oldNote)
+ update(note,oldNote)
 
-// }
+}
